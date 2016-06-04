d6ecc0da968791b95e080e23bd711644440b8bcc
Sebastian Lozano Herrera <sebaslh12@users.noreply.github.com>
2016-06-03 19:56:57 -0500

    Segue added

---
+++ ProyectoIOS/ProyectoIOS/Pods/Restofire/Sources/Configuration.swift
@@ -0,0 +1,65 @@
+//
+//  Configuration.swift
+//  Restofire
+//
+//  Created by Rahul Katariya on 24/03/16.
+//  Copyright © 2016 AarKay. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+/// A Configuration of RESTful Services. You must provide a `baseURL`.
+///
+/// ```swift
+/// var configuration = Configuration()
+/// configuration.baseURL = "http://www.mocky.io/v2/"
+/// configuration.headers = ["Content-Type": "application/json"]
+/// configuration.authentication.credential = NSURLCredential(user: "user", password: "password", persistence: .ForSession)
+/// configuration.validation.acceptableStatusCodes = [200..<300]
+/// configuration.validation.acceptableContentTypes = ["application/json"]
+/// configuration.logging = true
+/// configuration.retry.retryErrorCodes = [NSURLErrorTimedOut,NSURLErrorNetworkConnectionLost]
+/// configuration.retry.retryInterval = 20
+/// configuration.retry.maxRetryAttempts = 10
+/// let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+/// sessionConfiguration.timeoutIntervalForRequest = 7
+/// sessionConfiguration.timeoutIntervalForResource = 7
+/// sessionConfiguration.HTTPAdditionalHeaders = Manager.defaultHTTPHeaders
+/// configuration.manager = Alamofire.Manager(configuration: sessionConfiguration)
+/// ```
+public struct Configuration {
+    
+    /// The base URL. `nil` by default.
+    public var baseURL: String!
+    
+    /// The HTTP Method. `.GET` by default.
+    public var method: Alamofire.Method = .GET
+    
+    /// The request parameter encoding. `.JSON` by default.
+    public var encoding: Alamofire.ParameterEncoding = .JSON
+    
+    /// The HTTP headers. `nil` by default.
+    public var headers: [String : String]?
+
+    /// The `Authentication`.
+    public var authentication = Authentication()
+    
+    /// The `Validation`.
+    public var validation = Validation()
+    
+    /// The `Retry`.
+    public var retry = Retry()
+    
+    /// The logging, if enabled prints the debug textual representation of the 
+    /// request when the response is recieved. `false` by default.
+    public var logging: Bool = false
+    
+    /// The Alamofire Manager. `Alamofire.Manager.sharedInstance` by default.
+    public var manager = Alamofire.Manager.sharedInstance
+    
+    /// The queue on which reponse will be delivered. `dispatch_get_main_queue()`
+    /// by default.
+    public var queue: dispatch_queue_t? = dispatch_get_main_queue()
+    
+}
---
+++ ProyectoIOS/ProyectoIOS/Pods/Restofire/Sources/RequestEventuallyOperation.swift
@@ -0,0 +1,61 @@
+//
+//  RequestEventuallyOperation.swift
+//  Restofire
+//
+//  Created by Rahul Katariya on 17/04/16.
+//  Copyright © 2016 AarKay. All rights reserved.
+//
+
+#if !os(watchOS)
+
+import Foundation
+import Alamofire
+
+/// A `RequestOperation`, when added to an `NSOperationQueue` moitors the 
+/// network reachability and executes the `Requestable` when the network
+/// is reachable.
+///
+/// - Note: Do not call `start()` directly instead add it to an `NSOperationQueue`
+/// because calling `start()` will begin the execution of work regardless of network reachability
+/// which is equivalant to `RequestOperation`.
+public class RequestEventuallyOperation<R: Requestable>: RequestOperation<R> {
+
+    private let networkReachabilityManager = NetworkReachabilityManager()
+    override init(requestable: R, completionHandler: (Response<R.Model, NSError> -> Void)?) {
+        super.init(requestable: requestable, completionHandler: completionHandler)
+        self.ready = false
+        networkReachabilityManager?.listener = { status in
+            switch status {
+            case .Reachable(_):
+                if self.pause {
+                    self.resume = true
+                } else {
+                    self.ready = true
+                }
+            default:
+                if self.finished == false && self.executing == false {
+                    self.ready = false
+                } else {
+                    self.pause = true
+                }
+            }
+        }
+        networkReachabilityManager?.startListening()
+    }
+    
+    override func handleErrorResponse(response: Response<R.Model, NSError>) {
+        if self.retryAttempts > 0 {
+            if response.result.error!.code == NSURLErrorNotConnectedToInternet {
+                self.pause = true
+            } else if self.requestable.retryErrorCodes.contains(response.result.error!.code) {
+                self.retryAttempts -= 1
+                self.performSelector(#selector(RequestOperation<R>.executeRequest), withObject: nil, afterDelay: self.requestable.retryInterval)
+            }
+        } else {
+           super.handleErrorResponse(response)
+        }
+    }
+
+}
+
+#endif
