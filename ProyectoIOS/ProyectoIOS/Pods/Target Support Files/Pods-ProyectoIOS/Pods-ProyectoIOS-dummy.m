923c08b22654f93f780e1436058ae6d69643f195
Sebastian Lozano Herrera <Sebastian Lozano Herrera>
2016-06-06 11:15:01 -0500

    Folder organization

--- ProyectoIOS/ProyectoIOS/Pods/Target Support Files/Pods-ProyectoIOS/Pods-ProyectoIOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProyectoIOS : NSObject
-@end
-@implementation PodsDummy_Pods_ProyectoIOS
-@end
--- ProyectoIOS/ProyectoIOS/Pods/Target Support Files/RestEssentials/RestEssentials-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RestEssentials : NSObject
-@end
-@implementation PodsDummy_RestEssentials
-@end
